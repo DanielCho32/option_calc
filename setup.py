7d9973e0e98df712d7d1aaf7986be7d8749b3b6e
DanielCho32 <cho.daniel32@gmail.com>
2025-07-06 03:05:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="option_calc",
-    version="0.1",
+    version="0.1.0",
+    description="European & American Options Pricing Calculator with Greeks",
+    author="Daniel Cho",
+    author_email="cho.daniel32@gmail.com",
+    license="MIT",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=[
+        "streamlit",
+        "numpy",
+        "scipy",
+        "matplotlib",
+    ],
 )
